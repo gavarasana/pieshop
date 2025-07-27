faedae4517f3ee0640c285abc6cb534e67000e90
Ravi Gavarasana <gavarasana@gmail.com>
2025-07-26 20:30:49 -0400

    Added component card and employee quick view components

---
+++ Ravi.PieShop/Components/EmployeeQuickView.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Ravi.PieShop.Shared.Domain;
+
+namespace Ravi.PieShop.Components
+{
+    public partial class EmployeeQuickView
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+
+        private Employee? _employee;
+
+        protected override void OnParametersSet()
+        {
+            _employee = Employee;
+        }
+
+        private void ClosePopup()
+        {
+            _employee = null;
+        }
+
+    }
+}
--- Ravi.PieShop/Components/Pages/EmployeeOverview.razor.cs
+++ Ravi.PieShop/Components/Pages/EmployeeOverview.razor.cs
@@ -5,10 +5,15 @@ namespace Ravi.PieShop.Components.Pages
     public partial class EmployeeOverview
     {
         private List<Employee>? _employees = default!;
+        private Employee? _selectedEmployee;
         protected async override Task OnInitializedAsync()
         {
             await Task.Delay(2000); // Simulate a delay for data loading
             _employees = Services.MockDataService.Employees;
+        }
+        private void ShowEmployeePopup(Employee employee)
+        {
+            _selectedEmployee = employee;
         }
     }
 }
