5958b7926f83c71cf41ac23d0964191b85d19995
gavarasana <gavarasana@gmail.com>
2025-08-14 18:24:38 -0400

    Added QuickGrid component (#4)
    Co-authored-by: Ravi Gavarasana <RGavarasana@evolent.com>

--- Ravi.PieShop/Components/Pages/EmployeeDetail.razor.cs
+++ Ravi.PieShop/Components/Pages/EmployeeDetail.razor.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.QuickGrid;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Ravi.PieShop.Contracts.Services;
+using Ravi.PieShop.Migrations;
 using Ravi.PieShop.Shared.Domain;
 
 namespace Ravi.PieShop.Components.Pages
@@ -12,12 +15,21 @@ namespace Ravi.PieShop.Components.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; } = default!;
 
+        [Inject]
+        public ILogger<EmployeeDetail> Logger { get; set; } = default!;
+
         [Parameter]
         public int EmployeeId { get; set; }
 
         private Employee? _employee = default!;
-        public List<TimeRegistration>? TimeRegistrations = [];
-        private int itemHeight = 100;
+        private List<TimeRegistration>? TimeRegistrations = [];
+
+        private IQueryable<TimeRegistration> _timeRegistrationsQuery = default!;
+        private int ItemCount = 0;
+
+        private PaginationState PaginationState =  new() { ItemsPerPage = 50 };
+
+        private int ItemHeight = 100;
 
         protected async override Task OnInitializedAsync()
         {
@@ -28,14 +40,21 @@ namespace Ravi.PieShop.Components.Pages
                 throw new InvalidOperationException($"Employee with ID {EmployeeId} not found.");
             }
 
-            TimeRegistrations = await EmployeeService.GetTimeRegistrationsForEmployeeAsync(EmployeeId) ?? [];
+            //TimeRegistrations = await EmployeeService.GetTimeRegistrationsForEmployeeAsync(EmployeeId) ?? [];
+
+            TimeRegistrations  = await EmployeeService.GetPagedTimeRegistrationsForEmployeeAsync(EmployeeId, 25, 0);
+            _timeRegistrationsQuery = (await EmployeeService.GetTimeRegistrationsForEmployeeAsync(EmployeeId)).AsQueryable();
+            ItemCount = _timeRegistrationsQuery.Count();
+
         }
 
 
         private async ValueTask<ItemsProviderResult<TimeRegistration>> LoadTimeRegistrationsAsync(ItemsProviderRequest request)
         {
+            Logger.LogInformation("Loading time registrations for employee with ID {EmployeeId} starting at index {StartIndex} with count {Count}", EmployeeId, request.StartIndex, request.Count);
             var totalCount = await EmployeeService.GetTotalTimeRegistrationsCountForEmployeeAsync(EmployeeId);
-            var numberofRegistrationsToLoad = Math.Min(request.Count, (totalCount - request.StartIndex) );
+            var numberofRegistrationsToLoad = request.Count; // Math.Min(request.Count, totalCount - (request.StartIndex * request.Count));
+            Logger.LogInformation("Loading {NumberofRegistrationsToLoad} time entries", numberofRegistrationsToLoad);
             var registrations = await EmployeeService.GetPagedTimeRegistrationsForEmployeeAsync(EmployeeId,  numberofRegistrationsToLoad, request.StartIndex);
             return new ItemsProviderResult<TimeRegistration>(registrations, totalCount);
         }
