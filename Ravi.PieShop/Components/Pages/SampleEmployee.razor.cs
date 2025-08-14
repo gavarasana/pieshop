c76e9a4d789fb084098eafcd1bc7a669e3dee08d
Ravi Gavarasana <RGavarasana@evolent.com>
2025-08-14 19:56:21 -0400

    Added sample two way binding components

---
+++ Ravi.PieShop/Components/Pages/SampleEmployee.razor.cs
@@ -0,0 +1,31 @@
+﻿using Ravi.PieShop.Shared.Domain;
+
+namespace Ravi.PieShop.Components.Pages
+{
+    public partial class SampleEmployee
+    {
+        public Employee Employee { get; set; }
+
+        protected override void OnInitialized()
+        {
+            // This method is called when the component is initialized.
+            // You can perform any setup or initialization logic here.
+            base.OnInitialized();
+            Employee = new()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com"
+            };
+
+        }
+
+        private void UpdateName()
+        {
+                       // This method can be called to update the employee's name.
+            // You can add logic here to change the employee's name.
+            Employee.FirstName = "Jane";
+            Employee.LastName = "Smith";
+        }
+    }
+}
